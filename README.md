87fda4731c29075ccffeb5fef2f0764116de4ff0
Kanchana Ranasinghe <kahnchana@gmail.com>
2020-11-11 21:16:48 +0400

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Template for CV
+
+To generate, run `./generate.sh`. This should create 3 PDF files (single page, medium length, full) of CVs with 
+various levels of verbosity. Files created under `output/`. 
+
+All latex code for the CV is within `KR_academic_cv.tex`.   
---
+++ generate.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+pdflatex -jobname=KR_resume KR_academic_cv.tex
+
+pdflatex -jobname=KR_cv "\AtBeginDocument{\toggletrue{medium}} \input{KR_academic_cv.tex}"
+
+pdflatex -jobname=KR_cv_full "\AtBeginDocument{\toggletrue{long}} \input{KR_academic_cv.tex}"
+
+if [[ ! -d "output" ]]; then
+  mkdir "output"
+fi
+
+mv *.pdf output/
